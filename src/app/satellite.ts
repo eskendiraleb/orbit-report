88b315041851dea17ab814015fc9a716433afa7c
Eskindir <eskendiraleb@gmail.com>
2020-12-22 15:26:39 -0600

    Last Assignment6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -6,9 +6,9 @@ operational: boolean;
 launchDate: string;
 constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
 this.name = name;
-this.orbitType = orbitType;
 this.type = type;
 this.operational = operational;
+this.orbitType = orbitType;
 this.launchDate = launchDate;
 }
 shouldShowWarning(){
