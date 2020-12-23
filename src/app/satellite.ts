a4428d79b4a55d6a6ef5bd8bc36703c263d00a4f
Eskindir <eskendiraleb@gmail.com>
2020-12-23 17:01:53 -0600

    Last Assignment6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,17 +1,19 @@
 export class Satellite {
-name: string;
-orbitType: string;
-type: string;
-operational: boolean;
-launchDate: string;
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
 constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
-this.name = name;
-this.type = type;
-this.operational = operational;
-this.orbitType = orbitType;
-this.launchDate = launchDate;
+//Assign object (satellite) properties. 
+      this.name = name;
+      this.type = type;
+      this.operational = operational;
+      this.orbitType = orbitType;
+      this.launchDate = launchDate;
 }
+
 shouldShowWarning(){
-    return this.type === "Space Debris";
-}
+    return this.type.toUpperCase() === "SPACE DEBRIS";
+     }
 }
